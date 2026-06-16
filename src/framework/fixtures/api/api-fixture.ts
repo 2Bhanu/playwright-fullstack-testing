63440bfe50e946b1769448e38be9faee07c9fd97
bbhaskar <bbhaskar@fortinet.com>
2026-06-16 18:22:54 +0530

    added api client as dependency to fixture

---
+++ src/framework/fixtures/api/api-fixture.ts
@@ -0,0 +1,28 @@
+import {
+    test as base,
+    expect,
+} from '@playwright/test';
+
+import { ApiClientManager } from '@/api/Clients/ApiClientManager';
+
+type ApiFixtures = {
+    apiClientManager: ApiClientManager;
+};
+
+export const test =
+    base.extend<ApiFixtures>({
+        apiClientManager: async (
+            { request },
+            use
+        ) => {
+
+            const manager =
+                new ApiClientManager(
+                    request
+                );
+
+            await use(manager);
+        },
+    });
+
+export { expect };
