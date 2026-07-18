9f20cf8ea5467b1aac4c79acc6b51fe3fa58cd3e
bbhaskar <bbhaskar@fortinet.com>
2026-07-18 20:42:15 +0530

    added login fixture

--- src/framework/fixtures/pageFixture.ts
+++ src/framework/fixtures/pageFixture.ts
@@ -1,17 +1,22 @@
 import { test as base, expect } from '@playwright/test';
 
 import CommonComponentsPage from '../pages/components/commonComponentsPage';
+import { LoginPage } from '../pages/pages/loginpage';
 
 
 
 type Fixtures = {
   commonComponentsPage: CommonComponentsPage;
+  loginPage: LoginPage;
 };
 
 export const test = base.extend<Fixtures>({
   commonComponentsPage: async ({ page }, use) => {
     await use(new CommonComponentsPage(page));
   },
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  }
 });
 
 export { expect };
