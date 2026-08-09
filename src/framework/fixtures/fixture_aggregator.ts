0e8ccffbf9d4a2f685909c5fb362242e1b8193f2
bbhaskar <bbhaskar@fortinet.com>
2026-08-09 22:50:13 +0530

    formatted fixture

--- src/framework/fixtures/fixture_aggregator.ts
+++ src/framework/fixtures/fixture_aggregator.ts
@@ -1,8 +1,9 @@
 import { mergeTests } from '@playwright/test';
 
-import { test as pageTest } from './pageFixture';
+
 import { test as apiTest } from './api/api-fixture';
 import { test as loggingTest } from './log_fixture';
+import { test as pageTest } from './pageFixture';
 
 export const test = mergeTests(
   pageTest,
